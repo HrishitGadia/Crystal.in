cb5137bb50ae35d1b4b76b7331d993f282b9d401
HrishitGadia <89633448+HrishitGadia@users.noreply.github.com>
2022-07-12 18:04:07 +0530

    Add files via upload

--- Store.js
+++ Store.js
@@ -38,11 +38,11 @@ function getdata() {
         Color01 = ProductData['Color01'];
         Color02 = ProductData['Color02'];
         Color03 = ProductData['Color03'];
-        Program = ' <div class="container" id="'+ProductId+'ShowAbout"><button class="noner" id="'+ProductId+'ShowA" onclick = "Close(this.id)"><i class="bi bi-x-lg"></i></button><div class="box one"> <div class="details"> <div class="topic">Description</div> <p>' + Description + '</p> <div class="rating">' + TStars.repeat(Stars) + '</div> <div class="price-box"> <div class="price">' + PriceAfter + '</div> </div> </div> <div class="button1"> <button onclick="submit(this.id)" id="' + ProductId + '">Add To Bag</button> </div> </div> <div class="box two"> <div class="image-box"> <div class="image"> <img src="' + Image01 + '" alt=""> </div> <div class="info"> <div class="name">' + Name + '</div> <div class="shipping">FREE SHIPPING</div> </div> </div> </div> </div>'
+        Program = ' <div class="container" id="' + ProductId + 'ShowAbout"><button class="noner" id="' + ProductId + 'ShowA" onclick = "Close(this.id)"><i class="bi bi-x-lg"></i></button><div class="box one"> <div class="details"> <div class="topic">Description</div> <p>' + Description + '</p> <div class="rating">' + TStars.repeat(Stars) + '</div> <div class="price-box"> <div class="price">' + PriceAfter + '</div> </div> </div> <div class="button1"> <button onclick="submit(this.id)" id="' + ProductId + '">Add To Bag</button> </div> </div> <div class="box two"> <div class="image-box"> <div class="image"> <img src="' + Image01 + '" alt=""> </div> <div class="info"> <div class="name">' + Name + '</div> <div class="shipping">FREE SHIPPING</div> </div> </div> </div> </div>'
         Item = document.getElementById('hoverer').innerHTML;
         document.getElementById('hoverer').innerHTML = Item + Program;
 
-        Code = '<div class="product-card"> <div class="logo-cart"> <button class="noner" id="'+ProductId+'Show" onclick = "View(this.id)"><i class="bi bi-bag"></i> View Product</button></div> <div class="main-images"> <img id="' + ProductId + 'img1" class="blue active xyz class123" src="' + Image01 + '" alt="blue"> <img id="' + ProductId + 'img2" class="pink xyz class123"" src="' + Image02 + '" alt="pink"> <img id="' + ProductId + 'img3" class="yellow xyz class123" src="' + Image03 + '" alt="yellow"></div> <br> <div class="shoe-details"> <span class="shoe_name" id="' + ProductId + 'Name">' + Name + '</span> <p id="' + ProductId + 'Des">' + Description + '</p> <div class="stars">' + TStars.repeat(Stars) + ' </div> </div> <div class="color-price"> <div class="color-option"> <span class="color">Colour:</span> <div class="circles"> <span class=" circle ' + Color01 + ' active" id="' + ProductId + 'C1" onclick="ChangeActive(this.id)"></span> <span class="circle ' + Color02 + '" id="' + ProductId + 'C2" onclick="ChangeActive(this.id)"></span> <span class="circle ' + Color03 + ' " id="' + ProductId + 'C3" onclick="ChangeActive(this.id)"></span></div> </div> <div class="price"> <span class="price_num" id="' + ProductId + 'PriceNum">' + PriceAfter + '</span> <span class="price_letter" id="' + ProductId + 'PriceLet">' + PriceBefore + ' Only</span> </div> </div> <div class="button"> <div class="button-layer"></div> <button class="btnbtn" onclick="submit(this.id)" id="' + ProductId + '">Add To Bag</button> </div> </div>';
+        Code = '<div class="product-card"> <div class="logo-cart"> <button class="noner" id="' + ProductId + 'Show" onclick = "View(this.id)"><i class="bi bi-bag"></i> View Product</button></div> <div class="main-images"> <img id="' + ProductId + 'img1" class="blue active xyz class123" src="' + Image01 + '" alt="blue"> <img id="' + ProductId + 'img2" class="pink xyz class123"" src="' + Image02 + '" alt="pink"> <img id="' + ProductId + 'img3" class="yellow xyz class123" src="' + Image03 + '" alt="yellow"></div> <br> <div class="shoe-details"> <span class="shoe_name" id="' + ProductId + 'Name">' + Name + '</span> <p id="' + ProductId + 'Des">' + Description + '</p> <div class="stars">' + TStars.repeat(Stars) + ' </div> </div> <div class="color-price"> <div class="color-option"> <span class="color">Colour:</span> <div class="circles"> <span class=" circle ' + Color01 + ' active" id="' + ProductId + 'C1" onclick="ChangeActive(this.id)"></span> <span class="circle ' + Color02 + '" id="' + ProductId + 'C2" onclick="ChangeActive(this.id)"></span> <span class="circle ' + Color03 + ' " id="' + ProductId + 'C3" onclick="ChangeActive(this.id)"></span></div> </div> <div class="price"> <span class="price_num" id="' + ProductId + 'PriceNum">' + PriceAfter + '</span> <span class="price_letter" id="' + ProductId + 'PriceLet">' + PriceBefore + ' Only</span> </div> </div> <div class="button"> <div class="button-layer"></div> <button class="btnbtn" onclick="submit(this.id)" id="' + ProductId + '">Add To Bag</button> </div> </div>';
         Item = document.getElementById('products').innerHTML;
         document.getElementById('products').innerHTML = Item + Code;
       }
@@ -50,7 +50,7 @@ function getdata() {
   });
 }
 getdata();
-
+var Id = localStorage.getItem('UserId');
 function submit(Element) {
   UserId = Element;
   console.info(UserId)
@@ -65,7 +65,7 @@ function submit(Element) {
   C2 = document.getElementById(UserId + "C2").classList[1];
   C3 = document.getElementById(UserId + "C3").classList[1];
 
-  firebase.database().ref('User Cart/' + UserId).set({
+  firebase.database().ref('users/' + Id + '/User Cart').push({
     Stars: 5,
     Name: PName,
     PriceBefore: PLet,
@@ -130,16 +130,16 @@ function ChangeActive(Object) {
   }
 }
 
-function View(Element){
-  UserItem = Element+'About';
+function View(Element) {
+  UserItem = Element + 'About';
   document.getElementById('products').style.display = "none";
   document.getElementById(UserItem).classList.add("show");
-  
+
 }
 
-function Close(Element){
-  UserItem = Element+'bout';
+function Close(Element) {
+  UserItem = Element + 'bout';
   document.getElementById('products').style.display = "block";
   document.getElementById(UserItem).classList.remove("show");
-  
+
 }
